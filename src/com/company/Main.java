2327dc85067fb7e2ca7d8644e64e125da19dcf47
adetoito <adetoito2@gmail.com>
2016-11-03 14:20:51 -0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,48 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        /*
+
+        == WRAPPER CLASSES ==
+
+        These are classes that convert data types into objects.
+        They wrap primitive data types into an object.
+
+        Four important ones:
+        Integer
+            Integer ic = 7; // note the capital.
+
+            int i = 10;
+            Integer ii = i; // turned into an object.
+        Double
+        Boolean
+        Character
+
+        -Autoboxing-
+
+        Automatic conversion between primitive data types and its respective
+        wrapper classes.
+
+        Not tested in the AP exam.
+
+        -Additional methods-
+
+        Converting int into String
+            public String toString(int i);
+
+        int i = 104;
+        String ss = Integer.toString(i);
+
+        An easier way...
+        Appending an int type into an empty String.
+
+        int j = 3;
+        String s = "" + j; // s is set to "3"
+
+         */
+
+    }
+}
